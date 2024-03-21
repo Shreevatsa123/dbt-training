ae90b1ce4dbb07310dae3c84cd79ba22e8d6fa3b
Shreevatsa Agnihotri <ratnam2001shree@gmail.com>
2024-03-21 12:57:55 +0000

    added macros for markup and limit data in dev env

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -1,22 +1,25 @@
 select o.orderid
-,o.orderdate
-,o.shipdate
-,o.shipmode,
-o.ordersellingprice - o.ordercostprice as oroderprofit,
-o.ordersellingprice,
- o.ordercostprice,
---from customer
-c.customername,
-c.customerid,
-c.segment,
-c.country,
---From raw product
-p.category,
-p.productid,
-p.productname,
-p.subcategory
+    ,o.orderdate
+    ,o.shipdate
+    ,o.shipmode
+    ,o.ordersellingprice - o.ordercostprice as orderprofit
+    ,o.ordersellingprice
+    ,o.ordercostprice
+    -- from customer
+    ,c.customername
+    ,c.customerid
+    ,c.segment
+    ,c.country
+    -- from raw product
+    ,p.category
+    ,p.productid
+    ,p.productname
+    ,p.subcategory
+    ,{{ markup('ordersellingprice','ordercostprice') }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
-on o.customerid = c.customerid
+    on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
-on o.productid = p.productid
+    on o.productid = p.productid
+
+{{limit_data_in_dev('orderdate')}}
